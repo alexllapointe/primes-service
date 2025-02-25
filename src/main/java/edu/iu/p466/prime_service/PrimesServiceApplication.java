d40d60003049ee74eeb09edd7c4cfdbff90dff3b
alexllapointe <alexanderllapointe@gmail.com>
2025-02-25 17:14:06 -0500

    new update

--- src/main/java/edu/iu/p466/prime_service/PrimesServiceApplication.java
+++ src/main/java/edu/iu/p466/prime_service/PrimesServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PrimeServiceApplication {
+public class PrimesServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PrimeServiceApplication.class, args);
+		SpringApplication.run(PrimesServiceApplication.class, args);
 	}
 
 }
