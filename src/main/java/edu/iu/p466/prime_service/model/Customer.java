255b93e67edf2de1fdfa9219a819dc84fe80ec87
alexllapointe <alexanderllapointe@gmail.com>
2025-02-25 18:26:25 -0500

    practicum 2 update

---
+++ src/main/java/edu/iu/p466/prime_service/model/Customer.java
@@ -0,0 +1,24 @@
+package edu.iu.p466.prime_service.model;
+
+public class Customer {
+
+    private String username;
+    private String password;
+
+    public Customer(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUserName() { return username; }
+
+    public void setUserName(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {return password;}
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ src/main/java/edu/iu/p466/prime_service/security/KeyGeneratorUtils.java
@@ -0,0 +1,30 @@
+package edu.iu.p466.prime_service.security;
+
+
+// import org.springframework.security.*;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
+public class KeyGeneratorUtils {
+
+    private KeyGeneratorUtils () {}
+
+    static KeyPair generateRsaKey(){
+        KeyPair keyPair;
+
+        try {
+
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair= keyPairGenerator.generateKeyPair();
+            
+        } catch (Exception e) {
+            throw new IllegalStateException();
+        }
+        return keyPair;
+    }
+
+
+
+}
