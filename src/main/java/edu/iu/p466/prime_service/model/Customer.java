0d63689e1c8b8e77998ef3c9abdf1dfd66ea8a75
alexllapointe <alexanderllapointe@gmail.com>
2025-03-05 14:23:14 -0500

    update

--- src/main/java/edu/iu/p466/prime_service/model/Customer.java
+++ src/main/java/edu/iu/p466/prime_service/model/Customer.java
@@ -1,14 +1,15 @@
 package edu.iu.p466.prime_service.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
 
 
 @Table
 @Entity
 public class Customer {
 
+    @Id
     private String username;
     private String password;
 
@@ -23,6 +24,8 @@ public class Customer {
     }
 
     public String getUsername() { return username; }
+
+    // public Long getId() { return id; }
 
     public void setUsername(String username) {
         this.username = username;
